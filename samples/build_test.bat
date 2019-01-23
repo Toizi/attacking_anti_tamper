c432755b07f0fcb2936dad3f83d5a0c4812c1e12
Marius Guggenmos <mariusguggenmos@gmail.com>
2019-01-23 17:05:20 +0100

    adjusted test build script to enable exit on tamper detection + disable aslr

--- samples/build_test.bat
+++ samples/build_test.bat
@@ -1,4 +1,7 @@
 
-out\windows\bin\next.exe test_tamper.c /MD /O2 /Zi -Wm,-nopresets,-anti-tamper,-anti-tamper-frequency=0,-anti-tamper-min-guards=1,-anti-tamper-no-integrity,-anti-tamper-debug /Fetest_tamper_debug.exe
+out\windows\bin\next.exe test_tamper.c /MD /O2 /Zi -Wm,-nopresets,-anti-tamper,-anti-tamper-frequency=0,-anti-tamper-min-guards=1,-anti-tamper-no-integrity,-anti-tamper-debug,-reopt,-anti-tamper-exit /Fetest_tamper_debug.exe
 
-out\windows\bin\next.exe test_tamper.c /MD /O2 /Zi -Wm,-nopresets,-anti-tamper,-anti-tamper-frequency=0,-anti-tamper-min-guards=1,-anti-tamper-no-integrity
+out\windows\bin\next.exe test_tamper.c /MD /O2 /Zi -Wm,-nopresets,-anti-tamper,-anti-tamper-frequency=0,-anti-tamper-min-guards=1,-anti-tamper-no-integrity,-reopt,-anti-tamper-exit
+
+editbin /DYNAMICBASE:NO test_tamper.exe
+editbin /DYNAMICBASE:NO test_tamper_debug.exe
