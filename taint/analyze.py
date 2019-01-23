3856b03fad8c5773d046d53c5c2236c2ed8c68cb
Marius Guggenmos <mariusguggenmos@gmail.com>
2019-01-23 17:05:52 +0100

    added initial version of taint checking

--- taint/analyze.py
+++ taint/analyze.py
@@ -15,7 +15,8 @@ except ImportError:
 
 
 DEBUG = True
-BASE_STACK = 0x9f000000
+# MAIN_END = 0x1400038A8 # test_tamper_debug
+MAIN_END = 0x140001B58 # test_tamper
 
 def dprint(*args, **kargs):
     if DEBUG:
@@ -58,7 +59,7 @@ def print_triton_memory_at_register(ctx, reg, size=0x40):
 
 
 def emulate(ctx, trace, saved_contexts, saved_memories):
-    # type: (TritonContext, List[Trace], List[], List[]) -> None
+    # type: (TritonContext, List[Trace], List[], List[]) -> Union[None, List[int]]
     old_pc = 0
     pc = trace[0].addr
     set_triton_context(ctx, saved_contexts[0], set_rip=True)
@@ -68,9 +69,10 @@ def emulate(ctx, trace, saved_contexts, saved_memories):
     next_saved_context = 1
     print('[*] Starting emulation at {:#x}'.format(pc))
     count = 0
-    monitored_addr = 0x00007ffeed9eb2d0
+    monitored_addr = 0x7FFCCBC5D070
     monitored_val = ctx.getConcreteMemoryAreaValue(monitored_addr, 8)
     print("[D] Monitored val start: {:#x}".format(u64(monitored_val)))
+    tainted_addrs = set()
     while pc:
         opcodes = ctx.getConcreteMemoryAreaValue(pc, 16)
 
@@ -89,11 +91,14 @@ def emulate(ctx, trace, saved_contexts, saved_memories):
                 elif t == OPCODE.X86.RDTSCP:
                     print('skipping rdtscp')
                     skip_inst = True
+                elif t == OPCODE.X86.RDRAND:
+                    print('skipping rdrand')
+                    skip_inst = True
                 else:
                     print('Instruction not supported: {}'.format(inst))
                     break
 
-            print("{0:07} {1}".format(count, inst))
+            print("{0:07} {1} {2}".format(count, inst, 'tainted' if inst.isTainted() else ''))
 
             if len(saved_contexts) > next_saved_context and count == saved_contexts[next_saved_context].instr_num:
                 print("[*] saved_context {}".format(next_saved_context))
@@ -116,15 +121,19 @@ def emulate(ctx, trace, saved_contexts, saved_memories):
 
             cond = pc == old_pc
 
-        cur_val = ctx.getConcreteMemoryAreaValue(monitored_addr, 8)
-        if cur_val != monitored_val:
-            print("[D] Monitored value changed: {:#x} => {:#x}".format(u64(monitored_val), u64(cur_val)))
-            monitored_val = cur_val
+        if inst.isTainted():
+            tainted_addrs.add(inst.getAddress())
+
+
+        # cur_val = ctx.getConcreteMemoryAreaValue(monitored_addr, 8)
+        # if cur_val != monitored_val:
+        #     print("[D] Monitored value changed: {:#x} => {:#x}".format(u64(monitored_val), u64(cur_val)))
+        #     monitored_val = cur_val
         count += 1
-        if pc == 0x7ffeed9a6e1c:
-            print_triton_context(ctx)
-            print_triton_memory_at_register(ctx, "r8")
-            print_triton_memory_at_register(ctx, "r9")
+        # if pc == 0x7ffccbb56e1c:
+        #     print_triton_context(ctx)
+        #     print_triton_memory_at_register(ctx, "rcx")
+        #     print_triton_memory_at_register(ctx, "r9")
         # if pc == 0x7ffee9eb5f5a:
         #     print_triton_context(ctx)
         #     print_triton_memory_at_register(ctx, "rax")
@@ -135,6 +144,9 @@ def emulate(ctx, trace, saved_contexts, saved_memories):
         #     print_triton_context(ctx)
         #     if pc == 0x7fff9e494224:
         #         print_triton_memory_at_register(ctx, "rax")
+        if pc == MAIN_END:
+            print('[+] Reached end of main {:#x}. Emulation done'.format(MAIN_END))
+            return tainted_addrs
 
 
         if pc != trace[count].addr:
@@ -144,9 +156,55 @@ def emulate(ctx, trace, saved_contexts, saved_memories):
             for i in range(10):
                 print('{:#018x}'.format(trace[count+i].addr))
             # set_triton_context(ctx, saved_contexts[next_saved_context])
+            print('[D] monitored addr/value from last memory dump')
+            for saved_mem in reversed(saved_memories[:next_saved_memory-1]):
+                if saved_mem.contains_addr(monitored_addr):
+                    print('[D] {:#018x} : {:#x}'.format(monitored_addr, u64(saved_mem.get_value(monitored_addr))))
+                    break
+            print('[D] monitored addr/value currently')
+            print('[D] {:#018x} : {:#x}'.format(monitored_addr, u64(ctx.getConcreteMemoryAreaValue(monitored_addr, 8))))
             break
-        # if count > 5000:
-        #     break
+
+    return None
+
+
+def setup_triton(modules_path):
+
+    modules = get_modules(modules_path)    
+    if not modules:
+        return
+
+    # context boilerplate
+    ctx = TritonContext()
+    ctx.setArchitecture(ARCH.X86_64)
+    ctx.setAstRepresentationMode(AST_REPRESENTATION.PYTHON)
+    ctx.enableSymbolicEngine(False)
+    ctx.enableTaintEngine(True)
+
+    # segment values seem to be addresses instead of offsets into GDT
+    # ctx.setConcreteRegisterValue(ctx.registers.gs, 0x2b)
+    # ctx.setConcreteRegisterValue(ctx.registers.fs, 0x53)
+
+    # set up modules
+    for module in modules:
+        with open(module.path, 'rb') as f:
+            data = f.read()
+            ctx.setConcreteMemoryAreaValue(module.start, data)
+            if module.start == 0x140001000:
+                print('[*] Tainting main module memory {:#x} - {:#x}'.format(module.start, module.end))
+                taint_size = 64
+                for i in range(module.start, module.end, taint_size):
+                    ctx.taintMemory(MemoryAccess(i, taint_size))
+
+    # ctx.concretizeAllMemory()
+    # ctx.concretizeAllRegister()
+
+    # set up stack
+    # ctx.setConcreteRegisterValue(ctx.registers.rbp, BASE_STACK)
+    # ctx.setConcreteRegisterValue(ctx.registers.rsp, BASE_STACK)
+
+    return ctx
+
 
 def get_modules(modules_path):
     module_paths = glob(modules_path + '*')
@@ -164,36 +222,6 @@ def get_modules(modules_path):
     return modules
 
 
-def setup_triton(modules_path):
-
-    modules = get_modules(modules_path)    
-    if not modules:
-        return
-
-    # context boilerplate
-    ctx = TritonContext()
-    ctx.setArchitecture(ARCH.X86_64)
-    ctx.setAstRepresentationMode(AST_REPRESENTATION.PYTHON)
-    ctx.enableSymbolicEngine(False)
-
-    # segment values seem to be addresses instead of offsets into GDT
-    # ctx.setConcreteRegisterValue(ctx.registers.gs, 0x2b)
-    # ctx.setConcreteRegisterValue(ctx.registers.fs, 0x53)
-
-    # set up modules
-    for module in modules:
-        with open(module.path, 'rb') as f:
-            data = f.read()
-            ctx.setConcreteMemoryAreaValue(module.start, data)
-
-    # ctx.concretizeAllMemory()
-    # ctx.concretizeAllRegister()
-
-    # set up stack
-    # ctx.setConcreteRegisterValue(ctx.registers.rbp, BASE_STACK)
-    # ctx.setConcreteRegisterValue(ctx.registers.rsp, BASE_STACK)
-
-    return ctx
 
 registers = ['rdi', 'rsi', 'rbp', 'rsp', 'rbx', 'rdx', 'rcx', 'rax',
     'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15',
@@ -234,6 +262,13 @@ class saved_memory_t():
         self.data = data[self.struct_size:self.struct_size + self.size]
         consumed_bytes = self.struct_size + self.size
         return consumed_bytes
+    
+    def contains_addr(self, addr):
+        return self.start_addr <= addr and (addr - self.start_addr) < self.size
+    
+    def get_value(self, addr):
+        data_start = addr - self.start_addr
+        return self.data[data_start:data_start + 8].tobytes()
 
 
 def read_saved_memories(fpath):
@@ -310,7 +345,13 @@ def main():
     ctx = setup_triton(os.path.join(input_path, 'modules/'))
     print('[+] setup triton context')
     sys.stdout.flush()
-    emulate(ctx, trace, saved_contexts, saved_memories)
+    tainted_addresses = emulate(ctx, trace, saved_contexts, saved_memories)
+    if tainted_addresses is None:
+        return
+    
+    print('[+] Tainted addresses:')
+    for addr in sorted(tainted_addresses):
+        print('{:#018x}'.format(addr))
 
 
 if __name__ == '__main__':
